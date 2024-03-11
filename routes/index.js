f793319fef73a82b5ec86ff40b025b963816c4f4
Danylo Shchetinin <mail.redoge@gmail.com>
2024-03-11 13:38:59 +0200

    Refactor routes to include Facebook insights, accounts, campaigns, ad sets, and ads. Update meta insights to Facebook insights.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const auth = require("./auth/auth");
 const authFacebook = require("./auth/facebookAuth");
 const users = require("./users");
 const profile = require('./profile');
-const metaInsights = require('./meta/insights');
+const facebookInsights = require('./facebook/insights');
+const facebookAccounts = require('./facebook/accounts');
+const facebookCampaign = require('./facebook/campaign');
+const facebookAdSets = require('./facebook/adSets');
+const facebookAds = require('./facebook/ads');
 // const admin = require("./admin");
 
 router.all("*", unhandledRejectionHandler);
@@ -12,7 +16,11 @@ router.use("/", auth);
 router.use("/auth/facebook", authFacebook);
 router.use("/users", users);
 router.use('/profiles', profile);
-router.use('/meta/insights', metaInsights);
+router.use('/facebook/insights', facebookInsights);
+router.use('/facebook/accounts', facebookAccounts);
+router.use('/facebook/campaigns', facebookCampaign);
+router.use('/facebook/adsets', facebookAdSets);
+router.use('/facebook/ads', facebookAds);
 // router.use("/admin", admin);
 
 module.exports = router;
